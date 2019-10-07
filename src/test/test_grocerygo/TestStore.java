10c426ec2ecaa535e71f137d918cc49bbb1311c3
bruce <brucec@hotmail.com>
2019-10-07 15:17:30 -0700

    modify methods to be specific for food or Non food

--- src/test/test_grocerygo/TestStore.java
+++ src/test/test_grocerygo/TestStore.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -39,7 +38,7 @@ public class TestStore {
     public void testInsertEmpty() {
         assertEquals(0, mystore.total_food());
         Food carrot = new Food();
-        mystore.insert(carrot);
+        mystore.insertFood(carrot);
         assertEquals(1, mystore.total_food());
     }
 
@@ -47,53 +46,53 @@ public class TestStore {
     public void testInsertNonEmpty() {
         assertEquals(0, mystore.total_food());
         Food carrot = new Food();
-        mystore.insert(carrot);
-        mystore.insert(carrot);
+        mystore.insertFood(carrot);
+        mystore.insertFood(carrot);
         assertEquals(2, mystore.total_food());
     }
 
     @Test
     public void testRemoveEmpty() {
         Food carrot = new Food();
-        mystore.remove("carrot");
+        mystore.removeFood("carrot");
         assertEquals(0, mystore.total_food());
     }
 
     @Test
     public void testRemoveNoDuplicates() {
 
-        mystore.insert(apple);
-        mystore.insert(carrot);
-        assertTrue(mystore.contains("carrot"));
-        mystore.remove("carrot");
-        assertFalse(mystore.contains("carrot"));
+        mystore.insertFood(apple);
+        mystore.insertFood(carrot);
+        assertTrue(mystore.containsFood("carrot"));
+        mystore.removeFood("carrot");
+        assertFalse(mystore.containsFood("carrot"));
     }
 
     @Test
     public void testRemoveYesDuplicates() {
-        mystore.insert(carrot);
-        mystore.insert(carrot);
-        mystore.insert(apple);
-        assertTrue(mystore.contains("carrot"));
-        mystore.remove("carrot");
-        assertTrue(mystore.contains("carrot"));
-        mystore.remove("carrot");
-        assertFalse(mystore.contains("carrot"));
+        mystore.insertFood(carrot);
+        mystore.insertFood(carrot);
+        mystore.insertFood(apple);
+        assertTrue(mystore.containsFood("carrot"));
+        mystore.removeFood("carrot");
+        assertTrue(mystore.containsFood("carrot"));
+        mystore.removeFood("carrot");
+        assertFalse(mystore.containsFood("carrot"));
         assertEquals(1, mystore.total_food());
     }
 
     @Test
     public void testEmptyContains() {
-        assertFalse(mystore.contains("pineapple"));
+        assertFalse(mystore.containsFood("pineapple"));
     }
 
     @Test
     public void testNonEmptyContains() {
-        mystore.insert(carrot);
-        mystore.insert(apple);
-        assertTrue(mystore.contains("carrot"));
-        assertTrue(mystore.contains("apple"));
-        assertFalse(mystore.contains("pear"));
+        mystore.insertFood(carrot);
+        mystore.insertFood(apple);
+        assertTrue(mystore.containsFood("carrot"));
+        assertTrue(mystore.containsFood("apple"));
+        assertFalse(mystore.containsFood("pear"));
     }
 
     @Test
@@ -103,25 +102,25 @@ public class TestStore {
 
     @Test
     public void testTotalFoodNonEmpty() {
-        mystore.insert(carrot);
-        mystore.insert(apple);
+        mystore.insertFood(carrot);
+        mystore.insertFood(apple);
         assertEquals(2, mystore.total_food());
-        mystore.remove("carrot");
+        mystore.removeFood("carrot");
         assertEquals(1, mystore.total_food());
     }
     @Test
     public void testPriceEmpty() {
-        assertTrue(mystore.sortPrice().isEmpty());
+        assertTrue(mystore.sortPriceFood().isEmpty());
     }
     @Test //!!!
     public void testSortPriceNonEmpty() {
         Food lettuce = new Food("lettuce", 2, 5, 3);
         Food carrot = new Food("carrot", 3, 10, 5);
         Food apple = new Food("apple", 7, 20,10);
-        mystore.insert(lettuce);
-        mystore.insert(apple);
-        mystore.insert(carrot);
-        mystore.sortPrice();
+        mystore.insertFood(lettuce);
+        mystore.insertFood(apple);
+        mystore.insertFood(carrot);
+        mystore.sortPriceFood();
         List<Food> temp = new ArrayList<Food>();
         temp.add(lettuce);
         temp.add(carrot);
