b00fcf1210b1a45a8a23b9ca0358a93323aa95a5
bruce <brucec@hotmail.com>
2019-10-09 20:43:05 -0700

    add 1 more item to food database in main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -11,18 +11,15 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Store bruceStore = new Store();
         bruceStore.loadFoodDataBase();
+        Food pear = new Food("pear", 1, 200, 300);
+        bruceStore.insertFood(pear);
         bruceStore.loadNonFoodDataBase();
         System.out.println("here's whats in bruce's stores food database");
         bruceStore.printListofFood(bruceStore.getFoods());
-        System.out.println("here's whats in bruce's stores non database");
+        System.out.println("here's whats in bruce's store's non food database");
         bruceStore.printListofNonFood(bruceStore.getNonFoods());
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter a number below: would you like to sort Food Items by Price? "
-                + "(1 means yes, 0 means no)");
-        int number = input.nextInt();
+        int number = getNumber();
         System.out.println("you entered " + number);
-
 
         if (number == 1) {
             bruceStore.sortPriceFood();
@@ -30,6 +27,14 @@ public class Main {
         } else if (number == 0) {
             System.out.println("you didn't want to sort");
         }
+    }
+
+    private static int getNumber() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number below: would you like to sort Food Items by Price? "
+                + "(1 means yes, 0 means no)");
+        return input.nextInt();
     }
 }
 
