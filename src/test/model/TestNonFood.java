98afa2d87214ee097332c388c6bfc6cd04dffa2f
bruce <brucec@hotmail.com>
2019-10-27 14:56:57 -0700

    finished testing non food class

---
+++ src/test/model/TestNonFood.java
@@ -0,0 +1,43 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class TestNonFood {
+    NonFood nonFood;
+
+    @BeforeEach
+    public void runBefore() {
+        nonFood = new NonFood("fork",5);
+    }
+
+    @Test
+    public void testFood(){
+        NonFood fork = new NonFood();
+        fork.setName("fork");
+        fork.setPrice(5);
+        assertEquals(fork.getPrice(),nonFood.getPrice());
+        assertEquals(fork.getName(),nonFood.getName());
+    }
+
+    @Test
+    public void testEqualsShouldBeTrue(){
+        NonFood fork = new NonFood();
+        fork.setName("fork");
+        fork.setPrice(5);
+        assertTrue(fork.equals(nonFood));
+    }
+
+    @Test
+    public void testEqualsShouldBeFalse(){
+        NonFood fork = new NonFood();
+        fork.setName("fo2rk");
+        fork.setPrice(5);
+        assertFalse(fork.equals(nonFood));
+    }
+
+
+}
